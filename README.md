663d130b3d7e5a5bd08a9ee096bf8ae99377b2cc
sow0731 <sow.ishihara@gmail.com>
2022-06-17 19:56:30 +0900

    日時生成メソッドをhelloメソッド内に移動/README.mdを修正

--- README.md
+++ README.md
@@ -10,6 +10,7 @@
 ## 1.SpringBootを使って画面にHelloWorldを表示してみる
 
 ### 実行結果
+
 ![スクリーンショット 2022-06-15 17 37 20](https://user-images.githubusercontent.com/103630732/174248485-1a88f9b2-7d00-4151-8862-c414f903a945.png)
 
 ## 3.HelloWorldを表示したプロジェクトに自分で考えた変更を加えてみる
@@ -28,8 +29,9 @@
 
 ### 修正内容
 
-・DateTimeFormatクラスを削除し、HelloWorldControllerに日付と時間を日本語表示するメソッドを記述
+・DateTimeFormatクラスを削除し、HelloWorldControllerに日付と時間を日本語表示するメソッドをhelloメソッド内に記述
 
 ### 修正後の実行結果
+
 ![スクリーンショット 2022-06-17 16 01 08](https://user-images.githubusercontent.com/103630732/174248354-a9940f21-76c1-465f-8d06-74af39f01c91.png)
 
--- src/main/java/com/helloworld/helloworld/HelloWorldController.java
+++ src/main/java/com/helloworld/helloworld/HelloWorldController.java
@@ -9,13 +9,13 @@ import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HelloWorldController {
-    
-    LocalDateTime today = LocalDateTime.now();
-    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒");
-    String dateTimeJp = dateTimeFormatter.format(today);
 
     @GetMapping("/hello")
     public String hello(Model model) {
+        LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒");
+        String dateTimeJp = dateTimeFormatter.format(today);
+
         model.addAttribute("message", "HelloWorld!今日の日付と時間はこちら");
         model.addAttribute("day", dateTimeJp);
         return "HelloWorld";
